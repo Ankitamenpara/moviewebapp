f02ee3d6955b1b64a9239a0a1e8d4ea5f47a7092
ankita <ankita@improwised.com>
2017-06-12 15:32:22 +0530

    a

--- moviewebapp/routes/index.js
+++ moviewebapp/routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res, next) {
 	
 
 router.get('/thelist', function(req, res){
+
 	
 	var MongoClient = mongodb.MongoClient;
 
@@ -21,21 +22,75 @@ router.get('/thelist', function(req, res){
 			console.log('unable to connect');
 		}else {
 			console.log('connect');
-			
-			var moviedata = db.collection('moviedata') 
-			moviedata.aggregate([{$group : {_id : "$year" ,movies : {$sum: 1}}}, { $sort : {_id : 1}}]).toArray(function(err, result) {
-			
-				if(err){
-					res.send(err);
-				}else if (result.length){
-					res.render('list',{"list": result});
-				}else {
-					res.send('no doc');
-				}
-				console.log(req.query.name);
+			var query = req.query;
+			res.send(query)
 
-				db.close();
-			});
+			if(res.query.name==="yearacc"){
+				var moviedata = db.collection('moviedata') 
+				moviedata.aggregate([{$group : {_id : "$year" ,movies : {$sum: 1}}}, { $sort : {_id : 1}}]).toArray(function(err, result) {
+			
+					if(err){
+						res.send(err);
+					}else if (result.length){
+						res.render('list',{"list": result});
+					}else {
+						res.send('no doc');
+					}
+					console.log(req.query.name);
+
+					db.close();
+				});
+			}else if(res.query.name==="yeardes"){
+
+				var moviedata = db.collection('moviedata') 
+				moviedata.aggregate([{$group : {_id : "$year" ,movies : {$sum: 1}}}, { $sort : {_id : 1}}]).toArray(function(err, result) {
+			
+					if(err){
+						res.send(err);
+					}else if (result.length){
+						res.render('list',{"list": result});
+					}else {
+						res.send('no doc');
+					}
+					console.log(req.query.name);
+
+					db.close();
+				});
+
+			}else if(res.query.name==="movieacc"){
+
+				var moviedata = db.collection('moviedata') 
+				moviedata.aggregate([{$group : {_id : "$year" ,movies : {$sum: 1}}}, { $sort : {_id : 1}}]).toArray(function(err, result) {
+			
+					if(err){
+						res.send(err);
+					}else if (result.length){
+						res.render('list',{"list": result});
+					}else {
+						res.send('no doc');
+					}
+					console.log(req.query.name);
+
+					db.close();
+				});
+			}else(res.query.name==="moviedes"){
+
+				var moviedata = db.collection('moviedata') 
+				moviedata.aggregate([{$group : {_id : "$year" ,movies : {$sum: 1}}}, { $sort : {_id : 1}}]).toArray(function(err, result) {
+			
+					if(err){
+						res.send(err);
+					}else if (result.length){
+						res.render('list',{"list": result});
+					}else {
+						res.send('no doc');
+					}
+					console.log(req.query.name);
+
+					db.close();
+				});
+			}
+
 		}
 	})
 })
